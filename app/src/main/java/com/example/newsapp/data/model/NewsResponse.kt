dfc72d842dbf27fa70ec221320b8b0408f8dbbde
Derek Brown <kirillbb.baranov2012@gmail.com>
2021-11-20 03:59:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/data/model/NewsResponse.kt
@@ -0,0 +1,6 @@
+package com.example.newsapp.data.model
+data class NewsResponse(
+    val articles: MutableList<Article>,
+    val status: String,
+    val totalResults: Int
+)
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.newsapp.util
+
+const val API_KEY = "0b4bee50b9014521a9ae3cea8c167d0e"
+const val BASE_URL = "https://newsapi.org"
+const val SEARCH_NEWS_TIME_DELAY = 500L
+const val QUERY_PAGE_SIZE = 20
